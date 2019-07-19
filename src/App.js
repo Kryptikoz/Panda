6488689ee1d320c7b896871cbfe12c2ec591a856
Chris Pitcher <cpitcher@netbuilder.com>
2019-07-19 09:43:46 +0100

    Removed Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ class App extends Component {
         return (
             <div className="App">
                 <h1>Hello 👋, welcome to 🐼 framework</h1>
-                <p>This is a test</p>
             </div>
         );
     }
